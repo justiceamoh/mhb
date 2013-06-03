6de0bf715eed9bb583b4eb62ef369f686e53b8bf
Justice Amoh <justiceamoh@gmail.com>
2013-06-03 18:23:57 -0400

    Added urls and improved scraper to remove punctuations

--- hymnscraper.py
+++ hymnscraper.py
@@ -2,9 +2,13 @@
 
 from urllib2 import urlopen
 from bs4 import BeautifulSoup
+from time import sleep
 from pprint import pprint
+from string import maketrans
+from string import punctuation
 
 BASE_URL = "http://cyberhymnal.org/"
+URLS_FILE = "url_list.txt"
 
 def make_soup(url):
     html = urlopen(url).read()
@@ -15,7 +19,10 @@ def get_category_hymns(category_url):
     soup = make_soup(category_url);
     category = soup.title.string
     entries = soup.find("div","single-entry")
+
     title = [hymn.string for hymn in entries.findAll("a")]
+    title = [x.lower() for x in title]
+    title = [x.tramslate(maketrans("",""), punctuation) for x in title]
 
     url = [BASE_URL + hymn.get('href') for hymn in entries.findAll("a")]
     url = [hymn.split('../')[0] + hymn.split('../')[1] for hymn in url]
@@ -28,6 +35,18 @@ def get_category_hymns(category_url):
     output[category[2:3]]=data
     return output
 
-category = get_category_hymns("http://cyberhymnal.org/ttl/ttl-a.htm")
-# pprint(titles)
-pprint(category)
+
+
+f = open("url_list.txt")
+lines = f.readlines()
+
+all_hymns = dict()
+
+for line in lines:
+    new = get_category_hymns(line.rstrip())
+    all_hymns.update(new)
+    sleep(1)
+
+
+
+pprint(all_hymns)
