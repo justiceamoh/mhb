facb3525ea71f98746cf7c9242a78f1ca9b8d6c8
Justice Amoh <justiceamoh@gmail.com>
2013-08-03 16:06:21 +0000

    coded up obtaining data from .mhb files

--- db_builder.py
+++ db_builder.py
@@ -2,18 +2,41 @@
 # Author: Justice Amoh
 # Description: Script to build sqlite3 database out of hymn files (.mhb)
 
-import sqlite3
+import sqlite3, glob, os
 
-conn =sqlite3.connect("mhb.db")
-cursor = conn.cursor()
+# conn =sqlite3.connect("mhb.db")
+# cursor = conn.cursor()
 
 #creating a table
-cursor.execute("""CREATE TABLE a (mhb integer, title text, author text, hymn text)""")
+# cursor.execute("""CREATE TABLE a (mhb integer, title text, author text, hymn text)""")
 
-with open ('datafile') as f:
-    data=f.read()
+os.chdir("data")
+for files in glob.glob("*.mhb"):
+	with open(files) as f:
+		url=f.readline().rstrip('\n')
+		title=f.readline().rstrip('\n')
+		author=f.readline().rstrip('\n')
+		dump=f.readline()
+		hymn=f.read()
 
-sql = "INSERT INTO a VALUES (511, 'Begone Unbelief', 'John Newton', '?')" 
+	print url
+	print title
+	print hymn
+	print "\n\n\n"
+# with open ('datafile') as f:
+#     data=f.read()
 
-cursor.execute(sql,[buffer(data)])
-conn.commit()
+# sql = "INSERT INTO a VALUES (511, 'Begone Unbelief', 'John Newton', '?')" 
+
+# cursor.execute(sql,[buffer(data)])
+# conn.commit()
+
+
+
+
+
+# import glob
+# import os
+# os.chdir("/mydir")
+# for files in glob.glob("*.txt"):
+#     print files
