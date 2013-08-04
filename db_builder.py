4a19f4fe7022d90fd8e5daf5b3eca2da3a23b69d
Justice Amoh <justiceamoh@gmail.com>
2013-08-04 07:05:23 +0000

    unicode string issues fixed by using python buffers

--- db_builder.py
+++ db_builder.py
@@ -2,26 +2,34 @@
 # Author: Justice Amoh
 # Description: Script to build sqlite3 database out of hymn files (.mhb)
 
-import sqlite3, glob, os
+
+import sqlite3, glob, os, sys
 
 database =sqlite3.connect("mhb.db")
 cursor = database.cursor()
 
+hymn='mic test one two, one two'
+
 #creating a table
+cursor.execute('drop table if exists a')
 cursor.execute("""CREATE TABLE a 
-	(mhb integer, title text, author text, hymn text)""")
+	(mhb integer primary key, title text, author text, url text, hymn text)""")
 
 os.chdir("data")
 for ifile in glob.glob("*.mhb"):
 	with open(ifile) as f:
+		ifile=ifile.rstrip('.mhb')
 		url=f.readline().rstrip('\n')
 		title=f.readline().rstrip('\n')
 		author=f.readline().rstrip('\n')
 		dump=f.readline()
 		hymn=f.read()
 
-		sql = "INSERT INTO a VALUES (?, ?, ?, ?)" 
-		values = [ifile.rstrip('.mhb'),title,author,buffer(hymn)]
+		# sql = "INSERT INTO a VALUES (?, ?, ?, ?)" 
+		# values = [ifile.rstrip('.mhb'),title,author,url]
+
+		sql = "INSERT INTO a VALUES (?, ?, ?, ?, ?)" 
+		values = [ifile, title, buffer(author), url, buffer(hymn)]
  		cursor.execute(sql,values)
 
 
