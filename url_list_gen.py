c7dc93cc8af79975f8deb19961d407856f75cf8b
zionshine <yestrup@gmail.com>
2013-08-09 00:31:53 +0000

    bug fix: changed wwww to http:// in url names

--- url_list_gen.py
+++ url_list_gen.py
@@ -6,7 +6,7 @@ import string
 def url_list_gen():
     alphabet = string.ascii_lowercase;
 
-    urls = ['www.cyberhymnal.org/ttl/ttl-'+str(i)+'.htm' for i in alphabet]
+    urls = ['http://cyberhymnal.org/ttl/ttl-'+str(i)+'.htm' for i in alphabet]
 
     f = open('url_list.txt','w')
     for i in urls:
