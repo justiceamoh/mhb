401518102b03fcbfd9ce4cd9a6a41dc0bec47ecd
zionshine <yestrup@gmail.com>
2013-08-10 04:00:00 +0000

    update: no hymn title begins with x

--- url_list_gen.py
+++ url_list_gen.py
@@ -5,6 +5,7 @@ import string
 
 def url_list_gen():
     alphabet = string.ascii_lowercase;
+    alphabet = alphabet[:23] + 'yz' # no hymn begins with x
 
     urls = ['http://cyberhymnal.org/ttl/ttl-'+str(i)+'.htm' for i in alphabet]
 
