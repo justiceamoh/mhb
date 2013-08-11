f6e6e5d916f4e039bc31f3fe7d8b7d51a96d7279
zionshine <yestrup@gmail.com>
2013-08-11 16:32:13 +0000

    update: can get titles and urls

--- hymnscraper.py
+++ hymnscraper.py
@@ -10,43 +10,99 @@ from string import punctuation
 BASE_URL = "http://cyberhymnal.org/"
 URLS_FILE = "url_list.txt"
 
+hymnGlobal = ""
 def make_soup(url):
     html = urlopen(url).read()
-    return BeautifulSoup(html, "lxml")
+    return BeautifulSoup(html)
 
 
-def get_category_hymns(category_url):
-    soup = make_soup(category_url);
-    category = soup.title.string
-    entries = soup.find("div","single-entry")
-
-    title = [hymn.string for hymn in entries.findAll("a")]
-    title = [x.lower() for x in title]
-    title = [x.tramslate(maketrans("",""), punctuation) for x in title]
-
-    url = [BASE_URL + hymn.get('href') for hymn in entries.findAll("a")]
-    url = [hymn.split('../')[0] + hymn.split('../')[1] for hymn in url]
-
-    data=[]
-    data.append(title)
-    data.append(url)
-
-    output=dict()
-    output[category[2:3]]=data
-    return output
+def get_titles():
+    global entries
+    global count
+    n = 0
+    title = []
+    for hymn in entries.findAll('a'):
+        n += 1
+        if hymn.string == None:
+            count = n
+            continue
+        title.append(hymn.string)    
+    titles= []
+    for i in range(len(title)):
+        titles.append(title[i])        
+    return titles
 
 
+def get_urls():
+    global entries
+    global count
+    n = 0    
+    url = [] 
+    for hymn in entries.findAll('a'):
+        n+=1
+        if n == count:
+            continue
+        url.append(hymn.get('href'))
+    newUrl = []
+    for i in url[:]:
+        newUrl.append(BASE_URL+i[3:])
+    return newUrl
 
-f = open("url_list.txt")
-lines = f.readlines()
+    
+def get_lyrics(url):
+    soup = make_soup(url)
+    lyrics = soup.find('div','lyrics')
+    s = ""
+    for i in lyrics.strings:
+        if i[:1] != '\r':
+            s +='\n'
+        s += i
+    lyrics = s[2:]
+    return lyrics
 
-all_hymns = dict()
+##def get_authors(url):
+    
+    
 
-for line in lines:
-    new = get_category_hymns(line.rstrip())
-    all_hymns.update(new)
-    sleep(1)
+def handler():    
+    f = open(URLS_FILE)
+    lines = f.readlines()
 
+    lyrics = []             # holds the lyrics of each hymn
+    titles = []           # list contain the title each hymn
+    urls = []
+    authors = []
+    global entries,count
+    count = 0
+    for line in lines:
+        soup = make_soup(line.rstrip())
+        entries = soup.find("div","single-entry")
+        titles.append(get_titles()) ##whn writing to file encode as ascii
+        urls.append(get_urls())
+        count = 0
+##    lyrics.append([get_lyrics(x) for x in urls[-1][:2]])
+    return (titles,urls)#,lyrics,author)
+    
+   # return str(lyrics[0].encode('utf8').decode('ascii','ignore'))
 
+(titles,urls) = handler()
 
-pprint(all_hymns)
+f = open('titles.txt','w')
+h = open('titleUrls.txt','w')
+j=k=0
+y=""
+for i in titles:
+    for x in i:
+        x = str(x.encode('utf8').decode('ascii','ignore'))
+        y = str(urls[j][k].encode('utf8').decode('ascii','ignore'))
+        f.write(x+'\n')
+        h.write(y+'\n')
+        k += 1
+    j += 1
+    k=0
+
+f.fush()
+f.close()
+h.flush
+h.close()
+
